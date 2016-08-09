46049f6e867da9a6658949a31659ab42f9fac22f
quangnd <quangnd.edu@gmail.com>
2016-08-09 12:49:08 +0700

    Finish Project

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var nunjucks = require('nunjucks');
 var express = require('express');
-var app = express();
+var commonFuncs = require('./utilities/commons');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
+var app = express();
 var PORT = process.env.PORT || 3001;
 
 nunjucks.configure('views', {
 	autoescapse: true,
 	express: app
+});
+
+app.get('/', function (req, res) {
+	res.render('index.html', { url: commonFuncs.getUrl(req) });
+});
+
+app.post('/get-file-size', upload.single('fileName'), function (req, res, next){
+	res.send({ size: req.file.size });
+	console.dir(req.file);
 });
 
 app.listen(PORT, function (err) {
